fc2e0caf2620e6154139be440a7a257134e1ea5e
Faissal <fhattou@simplon.co>
2019-02-28 09:17:02 +0100

    Add vuetify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import App from './components/App.vue'
+import Vue from "vue";
+import App from "./components/App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
